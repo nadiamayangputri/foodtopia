0d2bd731a66a8d439928a27458472a7ecab3ca1e
Chengyao Xu <Chengyaox@student.unimelb.edu.au>
2018-04-16 15:35:58 +1000

    add wordsearch game

--- routes/home.js
+++ routes/home.js
@@ -5,4 +5,6 @@ const controller = require('../controller/controller');
 
 router.get('/', controller.home);
 router.get('/games/match', controller.match_game);
+router.get('/lookup', controller.lookup);
+router.get('/games/wordsearch', controller.wordsearch)
 module.exports = router;
