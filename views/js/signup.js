e6040bb1633681bc0a8c72bf088c5df2694b6aa5
Qianqian Zheng <audrey@Qianqian-Air.gateway>
2018-04-17 23:48:23 +1000

    signup and log in

---
+++ views/js/signup.js
@@ -0,0 +1,18 @@
+// Get the modal
+var modal = document.getElementById('id01');
+var modal2 = document.getElementById('id02');
+var btn = document.getElementById('signup');
+var loginbtn = document.getElementById('login');
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+btn.onclick = function() {
+    modal.style.display = "block";
+};
+
+loginbtn.onclick = function() {
+    modal2.style.display = "block";
+};
