e2fe7d4217b8ea03515243a33d3132092ed1f43d
Qianqian Zheng <audrey@Qianqian-Air.gateway>
2018-04-18 23:18:15 +1000

    valid links to buttons

--- routes/home.js
+++ routes/home.js
@@ -4,10 +4,12 @@ const controller = require('../controller/controller');
 
 
 router.get('/', controller.home);
-router.get('/profile', controller.profile)
+router.get('/profile', controller.profile);
+
 router.get('/games', controller.games);
 router.get('/games/match', controller.match_game);
 router.get('/games/wordsearch', controller.wordsearch);
+
 router.get('/lookup', controller.lookup);
 router.get('/lookup/searchresult', controller.searchresult);
 router.get('/information', controller.information);
