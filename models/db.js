d349c127f4f4d7e07296adb2b23c98af6454ef86
qianqian1535 <qianqianz2@student.unimelb.edu.au>
2018-05-01 12:21:24 +1000

    mlab db

---
+++ models/db.js
@@ -0,0 +1,15 @@
+//Import the mongoose module
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+const mongoDB = 'mongodb://qianqian:foodtopia19@ds163294.mlab.com:63294/foodtopia';
+
+mongoose.connect(mongoDB,function (err) {
+    if (err) {
+        console.log('fail to connect to mongo.');
+    }else{
+
+        console.log('successfully connected to mongo.');
+    }
+});
+require('./users');
