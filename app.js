6977922cfcd80d7fb245168cde8526f89d8f95ba
nadiaandyani <nadiaandyani@outlook.com>
2018-05-02 21:04:21 +1000

    app.js added

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 
+require('./models/db.js');
 const router =  require('./routes/home.js');
 
 app.set('view engine', 'ejs');
 app.use(router);
 app.use(express.static(__dirname + '/views'));
-// app.use(express.static(__dirname + '/views/js'));
-// app.use('/static', express.static(__dirname + '/icon/KR_Fruity'));
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function(){
