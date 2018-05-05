dd44b3d1bc688aa31a4e9d2237f7feeebd1aac16
qianqian1535 <qianqianz2@student.unimelb.edu.au>
2018-05-06 00:39:34 +1000

    minor fix

--- routes/home.js
+++ routes/home.js
@@ -18,10 +18,8 @@ router.get('/lookup', controller.lookup);
 router.get('/lookup/searchresult', controller.searchresult);
 router.get('/information', controller.information);
 
-router.get('/ingredients',controller.findAllIngredients);
-router.get('/ingredients/:name',controller.findIngredientByName);
-router.get('/ingredients/:category',controller.findIngredientByCategory);
-router.get('/name/:name',ingredientController.findIngredientByName);
-router.get('/api',ingredientController.findAllIngredients);
+router.get('/ingredients',ingredientController.findAllIngredients);
+router.get('/ingredients/:name',ingredientController.findIngredientByName);
+//router.get('/ingredients/:category',ingredientController.findIngredientByCategory);
 
 module.exports = router;
