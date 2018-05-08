5337a458d6efc88424ca2f56d5f4f6858220e998
Chengyao Xu <chengyaox@student.unimelb.edu.au>
2018-05-08 17:01:30 +1000

    implement information page and link the read more button to information page

--- controller/ingredients.js
+++ controller/ingredients.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
+var unirest = require('unirest');
 var ingredients = mongoose.model('ingredients');
+
 var findAllIngredients = function(req,res){
     ingredients.find(function(err,ingredients){
         if(!err){
@@ -12,15 +14,45 @@ var findAllIngredients = function(req,res){
 
 var findIngredientByName = function(req, res){
     var ingredientName = req.params.name;
-    ingredients.find({name:ingredientName},function(err,ingredientName){
-        if(!err){
-            res.send(ingredientName);
-        } else{
-            res.sendStatus(404);
-        }
-    });
+    var str1 = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=";
+    var str2 = ingredientName;
+    var str3 = "&limitLicense=false&number=5&ranking=1";
+    var url = str1.concat(str2,str3);
+    unirest.get(url)
+        .header("X-Mashape-Key", "Pc0JAnEGJnmshFERnSOKuLobbeqLp1YmLLrjsnvnWBFpsIX6eZ")
+        .header("Accept", "application/json")
+        .end(function (result) {
+            /*
+            for (var i = 0; i < 3; i++) {
+                var str1 = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/";
+                var str2 = result.body[i]["id"];
+                var str3 = "/information?includeNutrition=false";
+                var url = str1.concat(str2, str3);
+                unirest.get(url)
+                    .header("X-Mashape-Key", "Pc0JAnEGJnmshFERnSOKuLobbeqLp1YmLLrjsnvnWBFpsIX6eZ")
+                    .header("Accept", "application/json")
+                    .end(function (result) {
+                        */
+            var url1 = "https://spoonacular.com/"+ result.body[0].title.replace(/\s+/g, '-').toLowerCase() + "-"+ result.body[0].id;
+            var url2 = "https://spoonacular.com/"+ result.body[1].title.replace(/\s+/g, '-').toLowerCase() + "-"+ result.body[1].id;
+            var url3 = "https://spoonacular.com/"+ result.body[4].title.replace(/\s+/g, '-').toLowerCase() + "-"+ result.body[4].id;
+            ingredients.find({name: ingredientName}, function (err, ingredients) {
+                            if (!err) {
+                                res.render("ejs/dictionary/information", {
+                                    ingredients: ingredients[0],
+                                    recipe1: result.body,
+                                    url1: url1,
+                                    url2: url2,
+                                    url3: url3
+                                });
+                            } else {
+                                res.sendStatus(404);
+                            }
+                       // });
+                    });
+            //}
+        });
 };
-
 
 var findIngredientByCategory = function(req, res){
     var ingredientCategory = req.params.category;
@@ -46,13 +78,45 @@ var searchIngredientByCategory = function(req, res){
 
 var allIngredients = function(req, res){
     ingredients.find(function(err, ingredients){
-        if(!err){
-            res.render('ejs/dictionary/lookup', {ingredients:ingredients});
-        }else{
-            res.sendStatus(404);
+    if (!err) {
+        res.render('ejs/dictionary/lookup', {ingredients: ingredients});
+    } else {
+        res.sendStatus(404);
         }
     });
+
 };
+
+function getrecipesbyFood(text) {
+    var str1 = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=";
+    var str2 = text;
+    var str3 = "&limitLicense=false&number=5&ranking=1";
+    var url = str1.concat(str2,str3);
+    unirest.get(url)
+        .header("X-Mashape-Key", "Pc0JAnEGJnmshFERnSOKuLobbeqLp1YmLLrjsnvnWBFpsIX6eZ")
+        .header("Accept", "application/json")
+        .end(function (result) {
+
+            for (var i=0; i<3; i++){
+                getrecipesbyID(result.body[i]["id"]);
+            }
+        });
+}
+
+function getrecipesbyID(text) {
+
+    var str1 = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/";
+    var str2 = text;
+    var str3 = "/information?includeNutrition=false";
+    var url = str1.concat(str2,str3);
+    unirest.get(url)
+        .header("X-Mashape-Key", "Pc0JAnEGJnmshFERnSOKuLobbeqLp1YmLLrjsnvnWBFpsIX6eZ")
+        .header("Accept", "application/json")
+        .end(function (result) {
+            list.push(result.body["sourceUrl"]);
+        });
+    //console.log(recipesUrl);
+}
 
 module.exports.findIngredientByName = findIngredientByName;
 module.exports.findAllIngredients = findAllIngredients;
